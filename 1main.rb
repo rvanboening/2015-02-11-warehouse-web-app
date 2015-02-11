3fbd70ec7994527d62f7e1c3eb95c14a5a1134c6
Rachel VanBoening <vanboeningrr@gmail.com>
2015-02-11 13:15:53 -0600

    Working with add product.

--- 1main.rb
+++ 1main.rb
@@ -9,5 +9,10 @@ get "/add" do
   erb :add
 end
 
+get "/add_product_results" do
+  erb :add_product_results
+end
+
+
 # params = Item.new(item_name, item_cat, item_loc, item_quant, item_price, item_desc)
 
